daa713b023d933887d4da104a0d37c61ad24804c
Jeff <zhsu@amwares.com>
2021-05-24 10:48:57 +0800

    完善数字输入。

--- src/main/java/cn/jeff/utils/Dialogs.kt
+++ src/main/java/cn/jeff/utils/Dialogs.kt
@@ -2,7 +2,6 @@ package cn.jeff.utils
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
-import javafx.scene.control.IndexRange
 import javafx.scene.control.TextField
 import tornadofx.*
 
@@ -14,19 +13,10 @@ import tornadofx.*
  * @param initialText 初始值
  */
 open class RestrictedTextField(private val validationRegex: Regex, initialText: String = "") : TextField(initialText) {
-	override fun replaceText(range: IndexRange?, text: String?) {
-		if (validate(text))
-			super.replaceText(range, text)
-	}
 
 	override fun replaceText(start: Int, end: Int, text: String?) {
 		if (validate(text))
 			super.replaceText(start, end, text)
-	}
-
-	override fun replaceSelection(replacement: String?) {
-		if (validate(replacement))
-			super.replaceSelection(replacement)
 	}
 
 	private fun validate(text: String?): Boolean =
@@ -34,7 +24,7 @@ open class RestrictedTextField(private val validationRegex: Regex, initialText: 
 }
 
 //class NumberTextField(initialText: String = "") : RestrictedTextField(Regex("[0-9.\\-]*"), initialText)
-class NumberTextField(initialText: String = "") : RestrictedTextField(Regex("[0-9]+"), initialText)
+class NumberTextField(initialText: String = "") : RestrictedTextField(Regex("[0-9]*"), initialText)
 
 fun UIComponent.inputNumber(
 		initVal: Int, title: String = "输入数字", prompt: String = "",
@@ -57,9 +47,13 @@ fun UIComponent.inputNumber(
 				alignment = Pos.CENTER
 				spacing = 20.0
 				button("确定") {
+					isDefaultButton = true
 					action {
-						close()
-						op(input.value.toInt())
+						val stringValue = input.value
+						if (stringValue.isInt()) {
+							close()
+							op(stringValue.toInt())
+						}
 					}
 				}
 				button("取消") {
